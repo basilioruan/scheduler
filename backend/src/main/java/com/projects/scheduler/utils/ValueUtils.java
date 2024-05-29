0aa13706b49011ff678b2939e6fff7ab877740b4
Ruan Basilio <ruan_basilio@outlook.com>
2024-05-28 22:01:53 -0300

    :ok_hand: style: fixing checkstyle reports

--- backend/src/main/java/com/projects/scheduler/utils/ValueUtils.java
+++ backend/src/main/java/com/projects/scheduler/utils/ValueUtils.java
@@ -2,13 +2,14 @@ package com.projects.scheduler.utils;
 
 import com.projects.scheduler.inbound.dtos.responses.PairIdLabelDTO;
 
-public class ValueUtils {
+public final class ValueUtils {
+
+	private ValueUtils() {
+		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+	}
 
 	public static PairIdLabelDTO buildPairIdLabel(Long id, String label) {
-		return PairIdLabelDTO.builder()
-				.id(id)
-				.label(label)
-				.build();
+		return PairIdLabelDTO.builder().id(id).label(label).build();
 	}
 
 }
